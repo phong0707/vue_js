1c339a7f3503b406fe4cd863e99c1939e7a0515e
Noy Mack <noymack@Noys-MacBook-Pro.local>
2025-08-20 12:06:48 +0700

    upate_20_1

--- todos/app.js
+++ todos/app.js
@@ -27,25 +27,38 @@ createApp({
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
       if (key === "todos") {
-        this.todos = JSON.parse(localStorage.getItem("todos"));
+        this.todos = JSON.parse(localStorage.getItem(key));
+      }
+      if (key === "completed") {
+        this.completed = JSON.parse(localStorage.getItem(key));
       }
     }
   },
   methods: {
+    updateTodosStorage() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+    updateCompletedStorage() {
+      localStorage.setItem("completed", JSON.stringify(this.completed));
+    },
     addTask(){
       this.todos.push(this.task)
       this.task = ""
-      localStorage.setItem('todos', JSON.stringify(this.todos));
+      this.updateTodosStorage()
+      
     },
     DeleteItem(index){
       this.todos.splice(index, 1)
+      this.updateTodosStorage()
     },
     completedItem(index){
      this.completed.push(this.todos[index]),
      this.DeleteItem(index)
+      this.updateCompletedStorage()
     },
     DeleteCompleted(index){
       this.completed.splice(index, 1)
+      this.updateCompletedStorage()
     }
   }
 }).mount('#app')
